793174fa2779ea94556a2fb43d0803dc669321f9
justina177 <justinaenenche@yahoo.com>
2023-04-15 06:50:57 +0100

    added errorHandler midddleware

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('Hello World')
+const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv').config();
+const { errorhandler } = require('./middleware/errorMiddleware.js')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extened: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler)
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
+
+// 41.00
